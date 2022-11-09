e76e126fcfcb31b73845407dc2d73fd85d14eeef
Andrea Simone Costa <andrysimo1997@gmail.com>
2022-11-09 07:16:01 +0000

    fix formatter

--- microc-parser/lib/parsing.ml
+++ microc-parser/lib/parsing.ml
@@ -2,23 +2,23 @@ open Lexing
 
 exception Syntax_error of Location.lexeme_pos * string
 
-
 let string_of_position lexbuf =
   let pos = lexbuf.lex_curr_p in
-  Printf.sprintf "%s: line %d: column %d" pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+  Printf.sprintf "%s: line %d: column %d" pos.pos_fname pos.pos_lnum
+    (pos.pos_cnum - pos.pos_bol + 1)
 
-let parse _scanner _lexbuf = 
-  try
-    Parser.program _scanner _lexbuf
-  with
+let parse _scanner _lexbuf =
+  try Parser.program _scanner _lexbuf with
   | Scanner.Lexing_error (pos, msg) ->
-    Printf.fprintf stderr "\n%s: Scanner.Lexing_error %s\n" (Location.show_lexeme_pos pos) msg;
-    exit(-1)
+      Printf.fprintf stderr "\n%s: Scanner.Lexing_error %s\n"
+        (Location.show_lexeme_pos pos)
+        msg;
+      exit (-1)
   | Syntax_error (pos, msg) ->
-    Printf.fprintf stderr "\n%s: Syntax_error (%s)\n" (Location.show_lexeme_pos pos) msg;
-    exit(-1)
+      Printf.fprintf stderr "\n%s: Syntax_error (%s)\n"
+        (Location.show_lexeme_pos pos)
+        msg;
+      exit (-1)
   | Parser.Error ->
-    Printf.fprintf stderr "\n%s: Parser.Error\n" (string_of_position _lexbuf);
-    exit(-1)
-
-    
+      Printf.fprintf stderr "\n%s: Parser.Error\n" (string_of_position _lexbuf);
+      exit (-1)
