5f086bc4f56572fa22468c363c162bb3eb2c0cbb
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-01-05 15:35:16 +0000

    wip

--- microc-parser/bin/rt-support.c
+++ microc-parser/bin/rt-support.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TO BE COMPLETED
 int getint(){
     char buffer[32];
     if(fgets(buffer, 32, stdin) == NULL)
       return 0;
     return atoi(buffer);
+}
+
+void print(int i) {
+  printf("%d", i);
 }
